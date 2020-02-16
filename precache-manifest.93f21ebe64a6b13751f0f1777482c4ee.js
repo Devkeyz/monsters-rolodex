032c07aa5778b9ab518f69e70216fa5af4ec9480
devkeyz <deflyo@gmail.com>
2020-02-16 16:43:39 +0200

    Updates

---
+++ precache-manifest.93f21ebe64a6b13751f0f1777482c4ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1163405d1b67cfe86c2e1ca1e40968a7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "249bdf0d820a8d02dc33",
+    "url": "/monsters-rolodex/static/css/main.bb01c92e.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "249bdf0d820a8d02dc33",
+    "url": "/monsters-rolodex/static/js/main.dc540980.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
